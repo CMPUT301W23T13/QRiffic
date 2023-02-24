b25de752cd89de14713dc1c87c6e68b18191e723
Kunal112003 <112599849+Kunal112003@users.noreply.github.com>
2023-02-24 15:13:13 -0700

    menu flow

--- QRiffic Application/app/src/main/java/com/example/qriffic/MainActivity.java
+++ QRiffic Application/app/src/main/java/com/example/qriffic/MainActivity.java
@@ -54,66 +54,58 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        Fragment fragment = null;
-//        Class fragmentClass = null;
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-////        if (id == R.id.action_profile) {
-////
-////            return true;
-//
-//        switch(id){
-//            case R.id.search_users:
-//                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_content_main,new SearchUser())
-//                        .commit();
-////                fragmentClass = SearchUser.class;
-//////                new SearchUser(
-//                return true;
-//        }
-//
-////        try {
-////            fragment = (Fragment) fragmentClass.newInstance();
-////        } catch (Exception e) {
-////            e.printStackTrace();
-////        }
-//
-//
-//
-//        return super.onOptionsItemSelected(item);
-
-//    }
-
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Fragment fragment = null;
+        Class fragmentClass = null;
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-        switch (item.getItemId()) {
+        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_profile) {
+//
+//            return true;
+
+        switch(id){
             case R.id.search_users:
-                fragment = new SearchUser();
-                break;
+                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_content_main,new SearchUser())
+                        .commit();
 
-            // add more cases for other menu items here
-
-            default:
-                return super.onOptionsItemSelected(item);
+                return true;
         }
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        Fragment currentFragment = fragmentManager.findFragmentById(R.id.profile_crt);
-        if (currentFragment == null || !currentFragment.getClass().equals(fragment.getClass())) {
-            // only replace the fragment if it's not already displayed
-        fragmentManager.beginTransaction().replace(R.id.profile_crt, fragment,null).commit();
-        }
 
-        return true;
+        return super.onOptionsItemSelected(item);
+
     }
+
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        Fragment fragment = null;
+//
+//        switch (item.getItemId()) {
+//            case R.id.search_users:
+//                fragment = new SearchUser();
+//                break;
+//
+//            // add more cases for other menu items here
+//
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        Fragment currentFragment = fragmentManager.findFragmentById(R.id.profile_crt);
+//        if (currentFragment == null || !currentFragment.getClass().equals(fragment.getClass())) {
+//            // only replace the fragment if it's not already displayed
+//        fragmentManager.beginTransaction().replace(, fragment,null).commit();
+//        }
+//
+//        return true;
+//    }
 
 
 
