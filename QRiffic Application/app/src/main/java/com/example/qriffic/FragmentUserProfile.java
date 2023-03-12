4a702abfff6a58a40b640755a4bc6fb2aafff68e
cubeof2 <cubeof2@gmail.com>
2023-03-12 01:03:54 -0700

    cleaned up java code for userprofile fragment --ready to be written!

--- QRiffic Application/app/src/main/java/com/example/qriffic/FragmentUserProfile.java
+++ QRiffic Application/app/src/main/java/com/example/qriffic/FragmentUserProfile.java
@@ -7,6 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,15 +18,9 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class FragmentUserProfile extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    ListView profileList;
+    ArrayAdapter<QRCode> pListAdapter;
+    ArrayList<QRCode> qrList;
 
     public FragmentUserProfile() {
         // Required empty public constructor
@@ -31,28 +29,17 @@ public class FragmentUserProfile extends Fragment {
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment UserProfile.
      */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentUserProfile newInstance(String param1, String param2) {
+    public static FragmentUserProfile newInstance() {
         FragmentUserProfile fragment = new FragmentUserProfile();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -61,4 +48,20 @@ public class FragmentUserProfile extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_user_profile, container, false);
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        profileList = view.findViewById(R.id.profileList);
+        pListAdapter = new ArrayAdapter<>(getContext(),R.layout.qr_dex_content, qrList);
+        profileList.setAdapter(pListAdapter);
+
+
+        //code to add QR codes to the list goes here
+
+
+
+    }
+
+
 }
