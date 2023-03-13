e649d116d3b260b6dce08bc7a085b9a0f579bf64
Ritwik Rastogi <rit7011503@gmail.com>
2023-03-13 00:31:35 -0600

    Added a sleep function to test the initial toolbar click and new menu  click method

--- QRiffic Application/app/src/androidTest/java/com/example/qriffic/MainActivityTest.java
+++ QRiffic Application/app/src/androidTest/java/com/example/qriffic/MainActivityTest.java
@@ -3,6 +3,8 @@ package com.example.qriffic;
 
 import static junit.framework.TestCase.*;
 
+import static java.lang.Thread.sleep;
+
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
@@ -50,7 +52,7 @@ public class MainActivityTest {
      * Test to see if the navigation menu takes us to the correct fragment
      */
     @Test
-    public void checkFragment() {
+    public void checkFragment() throws InterruptedException { //Remove the throws InterruptedException if you don't want to use sleep()
         solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
         Activity currentActivity = solo.getCurrentActivity();
         FragmentManager fragmentManager = currentActivity.getFragmentManager();
@@ -58,37 +60,38 @@ public class MainActivityTest {
 
         // check if qr dex fragment is visible
         solo.clickOnActionBarItem(R.id.toolbar);
-        solo.clickInList(1);
+        sleep(5000);
+        solo.clickOnView(solo.getView(R.id.qr_dex));
         fragment = fragmentManager.findFragmentById(R.id.fragment_qrdex);
         assertTrue(fragment != null && fragment.isVisible());
 
         // check if user profile fragment is visible
         solo.clickOnActionBarItem(R.id.toolbar);
-        solo.clickInList(2);
+        solo.clickOnView(solo.getView(R.id.action_profile));
         fragment = fragmentManager.findFragmentById(R.id.fragment_user_profile);
         assertTrue(fragment != null && fragment.isVisible());
 
         // check if leaderboard fragment is visible
         solo.clickOnActionBarItem(R.id.toolbar);
-        solo.clickInList(3);
+        solo.clickOnView(solo.getView(R.id.leaderboard));
         fragment = fragmentManager.findFragmentById(R.id.fragment_leaderboard);
         assertTrue(fragment != null && fragment.isVisible());
 
         // check if search users fragment is visible
         solo.clickOnActionBarItem(R.id.toolbar);
-        solo.clickInList(4);
+        solo.clickOnView(solo.getView(R.id.search_users));
         fragment = fragmentManager.findFragmentById(R.id.fragment_search_user);
         assertTrue(fragment != null && fragment.isVisible());
 
         // check if scan qr fragment is visible
         solo.clickOnActionBarItem(R.id.toolbar);
-        solo.clickInList(5);
+        solo.clickOnView(solo.getView(R.id.scan_QR));
         fragment = fragmentManager.findFragmentById(R.id.fragment_temp_add_qr);
         assertTrue(fragment != null && fragment.isVisible());
 
         // check if map fragment is visible
         solo.clickOnActionBarItem(R.id.toolbar);
-        solo.clickInList(6);
+        solo.clickOnView(solo.getView(R.id.map));
         fragment = fragmentManager.findFragmentById(R.id.fragment_map);
         assertTrue(fragment != null && fragment.isVisible());
     }
