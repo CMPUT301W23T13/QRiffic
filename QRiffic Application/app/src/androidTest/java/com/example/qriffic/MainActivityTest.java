46ec29bac10ffc3f16db873ead1a4653c41016cd
Garrett Quesnel <g.quesnel@live.com>
2023-03-12 19:00:50 -0600

    added robotium stuff to gradle files, started the testing file

---
+++ QRiffic Application/app/src/androidTest/java/com/example/qriffic/MainActivityTest.java
@@ -0,0 +1,47 @@
+package com.example.qriffic;
+
+
+import static junit.framework.TestCase.*;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.robotium.solo.Solo;
+
+import org.junit.*;
+
+public class MainActivityTest {
+
+    private Solo solo;
+
+    @Rule
+    public ActivityTestRule<MainActivity> rule =
+            new ActivityTestRule<>(MainActivity.class, true, true);
+
+    /**
+     * Runs before all tests and creates solo instance.
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+    }
+
+    @Test
+    public void start() throws Exception {
+        MainActivity activity = rule.getActivity();
+    }
+
+    @Test
+    public void checkActivity() {
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+    }
+
+
+
+
+    @After
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+    }
+}
