9799c3db1e0de7445ffba48bcf639d55ebe2fb17
Garrett Quesnel <g.quesnel@live.com>
2023-03-17 22:19:49 -0600

    fixed some permissions issues

--- QRiffic Application/app/src/main/java/com/example/qriffic/FragmentScanner.java
+++ QRiffic Application/app/src/main/java/com/example/qriffic/FragmentScanner.java
@@ -1,10 +1,16 @@
 package com.example.qriffic;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.Manifest;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.journeyapps.barcodescanner.BarcodeCallback;
@@ -14,6 +20,17 @@ import com.journeyapps.barcodescanner.DecoratedBarcodeView;
 public class FragmentScanner extends Fragment {
 
     private DecoratedBarcodeView barcodeView;
+
+    // camera permission stuff (see onResume())
+    private final ActivityResultLauncher<String> requestPermission = registerForActivityResult(
+            new ActivityResultContracts.RequestPermission(),
+            isGranted -> {
+                if (isGranted) {
+                    barcodeView.resume();
+                } else {
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, 1);
+                }
+            });
 
     private BarcodeCallback callback = new BarcodeCallback() {
 
@@ -37,6 +54,13 @@ public class FragmentScanner extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_scanner, container, false);
+
+    // check if the app has permission to access the device's location for QRCode generation
+        if (ContextCompat.checkSelfPermission(requireActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // if not, request permission
+            ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+
         barcodeView = view.findViewById(R.id.barcode_scanner);
         barcodeView.decodeContinuous(callback);
 
@@ -46,7 +70,7 @@ public class FragmentScanner extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        barcodeView.resume();
+        requestPermission.launch(Manifest.permission.CAMERA);
     }
 
     @Override
