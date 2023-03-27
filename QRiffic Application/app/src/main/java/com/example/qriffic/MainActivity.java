d42507862aba7fce180becb63d97eea0a853af69
Ritwik Rastogi <rit7011503@gmail.com>
2023-03-27 15:53:13 -0600

    Disabled backswipe and backbutton

--- QRiffic Application/app/src/main/java/com/example/qriffic/MainActivity.java
+++ QRiffic Application/app/src/main/java/com/example/qriffic/MainActivity.java
@@ -55,6 +55,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onBackPressed() {}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
